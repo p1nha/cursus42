eccd456a76376810736892fb82fcf8c53d0e5b45
alfrberm <alfrberm@student.42madrid.com>
2024-09-19 15:07:21 +0200

    strncmp hecha

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,27 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alfrberm <alfrberm@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/19 13:39:08 by alfrberm          #+#    #+#             */
-/*   Updated: 2024/09/19 14:58:13 by alfrberm         ###   ########.fr       */
+/*   Created: 2024/08/13 18:11:09 by alfrberm          #+#    #+#             */
+/*   Updated: 2024/09/19 15:04:54 by alfrberm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrchr(const char *str, int c)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
-	i = ft_strlen((char *)str);
-	while (i > 0)
+	i = 0;
+	while (s1[i] != 0 && s2[i] != 0 && i < n)
 	{
-		if (str[i] == c)
-			return ((char *)&str[i]);
-		i--;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
+	if (i < n)
+		return (s1[i] - s2[i]);
 	return (0);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -1,27 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alfrberm <alfrberm@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/19 13:39:08 by alfrberm          #+#    #+#             */
-/*   Updated: 2024/09/19 14:58:13 by alfrberm         ###   ########.fr       */
+/*   Created: 2024/09/19 15:05:34 by alfrberm          #+#    #+#             */
+/*   Updated: 2024/09/19 15:05:35 by alfrberm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char	*ft_strrchr(const char *str, int c)
-{
-	int	i;
-
-	i = ft_strlen((char *)str);
-	while (i > 0)
-	{
-		if (str[i] == c)
-			return ((char *)&str[i]);
-		i--;
-	}
-	return (0);
-}
